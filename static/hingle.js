9a0c4aecfe253f5373a52ea1ba464277df6f6b47
XiaMuqingyuan <qq2279402318@gmail.com>
2024-06-20 01:05:17 +0800

    Site updated: 2024-06-20 01:05:17

--- static/hingle.js
+++ static/hingle.js
@@ -174,7 +174,7 @@ var Paul_Hingle = function (config) {
   if (config.copyright) {
     document.oncopy = function () {
       ks.notice("复制内容请注明来源并保留版权信息！", {
-        color: "#40E0D0",
+        color: "pink",
         overlay: true,
       });
     };
--- static/kico.js
+++ static/kico.js
@@ -416,5 +416,5 @@ Array.prototype.remove = function (value) {
 
     global.ks = KStyle;
 
-    console.log("%c Kico Style %c https://paugram.com ", "color: #fff; margin: 1em 0; padding: 5px 0; background: #3498db;", "margin: 1em 0; padding: 5px 0; background: #efefef;");
+    console.log("%c Kico Style %c https://xxs.us.kg ", "color: #fff; margin: 1em 0; padding: 5px 0; background: #3498db;", "margin: 1em 0; padding: 5px 0; background: #efefef;");
 })(window);
