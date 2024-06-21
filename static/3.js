62c579e80fe43fb4ba3697d1f82dd33b60bac7a1
XiaMuqingyuan <qq2279402318@gmail.com>
2024-06-21 17:24:10 +0800

    Site updated: 2024-06-21 17:24:10

--- static/3.js
+++ static/3.js
@@ -1,6 +1,12 @@
 let titleTime, OriginTitile = document.title;
-document.addEventListener("visibilitychange", (function () {
-    document.hidden ? (document.title = "w(ﾟДﾟ)w 不要走！再看看嘛！", clearTimeout(titleTime)) : (document.title = "♪(^∇^*)欢迎肥来！", titleTime = setTimeout((function () {
-        document.title = OriginTitile
-    }), 2e3))
-}));
+document.addEventListener("visibilitychange",
+    (function () {
+        document.hidden ?
+            (document.title = "梧桐叶上三更雨,叶叶声声是别离",
+                clearTimeout(titleTime)) :
+            (document.title = "无可奈何花落去,似曾相识燕归来",
+                titleTime = setTimeout(
+                    (function () {
+                        document.title = OriginTitile
+                    }), 2e3))
+    }));
