9bb7847e76674de884ee6e4228a59ac5b93150ad
XiaMuqingyuan <qq2279402318@gmail.com>
2025-10-27 03:13:16 +0800

    Site updated: 2025-10-27 03:13:16

--- static/3.js
+++ static/3.js
@@ -2,7 +2,7 @@ let titleTime, OriginTitile = document.title;
 document.addEventListener("visibilitychange",
     (function () {
         document.hidden ?
-            (document.title = "不在看看吗？",
+            (document.title = "不再看看吗？",
                 clearTimeout(titleTime)) :
             (document.title = "欢迎回来！",
                 titleTime = setTimeout(
